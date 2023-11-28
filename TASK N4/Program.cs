4561927bc59f4bff79a8fad462922ca7d8f5eeb1
FrameworkTV <ioanejaniashvili19@gmail.com>
2023-11-29 01:29:01 +0400

    new changes for task 3

--- TASK N4/Program.cs
+++ TASK N4/Program.cs
@@ -5,20 +5,15 @@
         static void Main(string[] args)
         {
             var numbers = new double[5];
-            Console.WriteLine("Enter first number:");
-            numbers[0] = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter second number:");
-            numbers[1] = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter third number:");
-            numbers[2] = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter fourth number:");
-            numbers[3] = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter fifth number:");
-            numbers[4] = Convert.ToDouble(Console.ReadLine());
-            var sumNumbers = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4];
-            Console.WriteLine("SUM of Numbers:" + sumNumbers);
-            var averageNumbers = sumNumbers / 5;
-            Console.WriteLine("Average of Numbers:" + averageNumbers);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Enter number");
+                numbers[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            var sum = numbers.Sum();
+            Console.WriteLine("Sum of Numbers:" + sum);
+            var averageSum = sum / numbers.Length;
+            Console.WriteLine("Average of numbers:" + averageSum);
             var maxNumber = numbers[0];
             var minNumber = numbers[0];
             for (int i = 0; i < numbers.Length; i++)
@@ -32,15 +27,14 @@
                     minNumber = numbers[i];
                 }
             }
-            Console.WriteLine("Max Number: " + maxNumber);
+            Console.WriteLine("Max Number is: " + maxNumber);
             Console.WriteLine("Min Number: " + minNumber);
-
             for (int i = 0; i < numbers.Length; i++)
             {
                 if (numbers[i] % 3 == 0)
                 {
-                    Console.WriteLine("divisibleByThree:" + numbers[i]);
                 }
+                Console.WriteLine("divisibleByThree:" + numbers[i]);
             }
         }
     }
