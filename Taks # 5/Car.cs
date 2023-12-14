df57b7e9adb2dc80e0c45e39ddfdfce7305d2bfc
FrameworkTV <ioanejaniashvili19@gmail.com>
2023-12-14 17:28:13 +0400

    Task 5

---
+++ Taks # 5/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taks___5
+{
+
+
+    public class Car : Vehicle, IStartable
+    {
+        public int NumberOfDoors { get; set; }
+        public decimal TrunkCapacity { get; set; }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Car Information:");
+            Console.WriteLine($"Make: {Make}");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Year: {Year}");
+            Console.WriteLine($"Price: {Price}");
+            Console.WriteLine($"Number of Doors: {NumberOfDoors}");
+            Console.WriteLine($"Trunk Capacity: {TrunkCapacity}");
+        }
+
+        public void Start()
+        {
+            Console.WriteLine($"Starting Car");
+        }
+    }
+}
+
+
---
+++ Taks # 5/Garage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taks___5
+{
+    public class Garage
+    {
+        public List<Vehicle> vehicles = new List<Vehicle>();
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            vehicles.Add(vehicle);
+        }
+
+        public void PrintAllVehicles()
+        {
+            foreach (var vehicle in vehicles)
+            {
+                vehicle.DisplayInfo();
+                Console.WriteLine();
+            }
+        }
+        public decimal CalculateTotalPrice()
+        {
+            decimal total = 0;
+            foreach (var vehicle in vehicles)
+            {
+                total += vehicle.Price;
+            }
+            return total;
+        }
+    }
+}
