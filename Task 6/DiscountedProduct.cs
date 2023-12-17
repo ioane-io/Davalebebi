be9d4ca46886199b565c557aca1e26611e7c95ff
FrameworkTV <ioanejaniashvili19@gmail.com>
2023-12-17 18:43:53 +0400

    task 6

---
+++ Task 6/DiscountedProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    public class DiscountedProduct : Product
+    {
+        public DiscountedProduct(string name, decimal price, ProductCategory category, int quantityInStock, decimal discount)
+       : base(name, price, category, quantityInStock, discount)
+        {
+        }
+        public override string GetProductInfo()
+        {
+            return $"Discounted {base.GetProductInfo()}";
+        }
+    }
+}
---
+++ Task 6/Inventory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    public class Inventory
+    {
+        private Dictionary<string, Item> items;
+
+        public Inventory()
+        {
+            items = new Dictionary<string, Item>();
+        }
+        public void DictionaryAddItem(string key, Item item)
+        {
+            if (items.ContainsKey(key))
+            {
+                throw new ArgumentException("Item already exists.");
+            }
+
+            items[key] = item;
+        }
+        public Item GetItem(string key)
+        {
+            if (items.ContainsKey(key))
+            {
+                return items[key];
+            }
+
+            throw new KeyNotFoundException("Item not found");
+        }
+        public decimal CalculateTotalPrice()
+        {
+            decimal totalPrice = 0;
+
+            foreach (var item in items.Values)
+            {
+                totalPrice += item.Price;
+            }
+            return totalPrice;
+        }
+    }
+}
